4570b3ede7e01b03bbfcea70bffc90c6abacbd23
marcelopython <msr88957@gmail.com>
2021-06-01 22:17:12 -0300

    Adicionado configuraçao incial para o vuejs

---
+++ vue-finances-front/src/router/index.js
@@ -0,0 +1,18 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import authRoutes from '../modules/auth/router'
+
+Vue.use(VueRouter)
+
+const routes = [
+  ...authRoutes,
+  { path: '', redirect: '/login' }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
