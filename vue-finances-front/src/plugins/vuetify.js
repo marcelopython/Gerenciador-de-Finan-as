12f321517e929585489ef77c79615ec277fbf2b3
marcelopython <msr88957@gmail.com>
2021-06-02 12:34:15 -0300

    Validaçao de login

--- vue-finances-front/src/plugins/vuetify.js
+++ vue-finances-front/src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 import pt from 'vuetify/lib/locale/pt'
+import colors from 'vuetify/lib/util/colors'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
@@ -13,5 +14,19 @@ export default new Vuetify({
   },
   icons: {
     iconfont: 'md'
+  },
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.teal.darken1,
+        accent: colors.indigo.darken1,
+        info: colors.blue,
+        error: colors.pink.darken2,
+        success: colors.teal.lighten1,
+        warning: colors.purple.darken1
+      }
+    }
   }
+
 })
