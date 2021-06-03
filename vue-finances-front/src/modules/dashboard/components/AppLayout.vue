1eb270e4b6c67b94080552c0df1d973fab9779f1
marcelopython <msr88957@gmail.com>
2021-06-03 18:14:17 -0300

    Menu toolbar

---
+++ vue-finances-front/src/modules/dashboard/components/AppLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <AppToolbar v-model="drawer" />
+    <AppMenu v-model="drawer" />
+  </div>
+</template>
+
+<script>
+import AppToolbar from './AppToolbar'
+import AppMenu from './AppMenu'
+
+export default {
+  name: 'AppLayout',
+  components: {
+    AppToolbar,
+    AppMenu
+  },
+  data: () => ({
+    drawer: false
+  })
+
+}
+</script>
---
+++ vue-finances-front/src/modules/dashboard/components/AppMenu.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-navigation-drawer
+    :value="value"
+    @input="$emit('input', $event)"
+    :mini-variant="mini"
+    absolute
+    temporary
+  >
+    <v-list>
+
+      <v-list-item
+        v-if="mini"
+        @click.stop="mini = !mini"
+      >
+
+        <v-toolbar-title>
+
+          <v-btn
+            icon
+            style="display: contents;"
+          >
+            <v-icon>chevron_right</v-icon>
+          </v-btn>
+
+        </v-toolbar-title>
+
+      </v-list-item>
+
+      <v-list-item tag="div">
+
+        <v-list-item-icon>
+          <v-icon>person</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-title>User Name</v-list-item-title>
+
+        <v-toolbar-title>
+          <v-btn
+            icon
+            @click.stop="mini = !mini"
+          >
+            <v-icon>chevron_left</v-icon>
+          </v-btn>
+        </v-toolbar-title>
+
+      </v-list-item>
+
+    </v-list>
+
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+  props: {
+    value: Boolean
+  },
+  data: () => ({
+    items: [
+      { title: 'Home', icon: 'dashboard', url: '/dashboard', exact: true }
+    ],
+    mini: false
+  })
+}
+</script>
